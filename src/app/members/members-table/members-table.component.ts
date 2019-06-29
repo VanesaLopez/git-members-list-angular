b8c5d43de8939781bd5a39012dd14d07b8d7177b
VanesaLopez <avanesalopez@hotmail.com>
2019-06-30 00:23:39 +0200

    Added nav footer and input

--- src/app/members/members-table/members-table.component.ts
+++ src/app/members/members-table/members-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PageChangedEvent } from 'ngx-bootstrap/pagination';
 
 import { MemberEntity } from '../models/member.model';
@@ -9,12 +9,18 @@ import { MembersApiService } from '../members-api.service';
   templateUrl: './members-table.component.html',
   styles: []
 })
-export class MembersTableComponent {
+export class MembersTableComponent implements OnInit {
   members: MemberEntity[] = [];
   returnedmembers: MemberEntity[]; 
   itemsPerPage = 3;
+  isLoading = false;
+  organization = 'lemoncode';
 
   constructor(private membersApi: MembersApiService) { }
+
+  ngOnInit(): void {
+    this.loadMembers(this.organization);
+  }
  
   pageChanged(event: PageChangedEvent): void {
     const startItem = (event.page - 1) * event.itemsPerPage;
@@ -22,12 +28,15 @@ export class MembersTableComponent {
     this.returnedmembers = this.members.slice(startItem, endItem);
   }
 
-  loadMembers() {
-    this.membersApi.getAllMembers('lemoncode')
+  loadMembers(organization: string) {
+    this.isLoading = true;
+    this.members = [];
+    this.membersApi.getAllMembers(organization)
       .subscribe((ms) => {
         this.members = ms
+        this.isLoading = false;
+        this.organization = organization;
         this.returnedmembers = this.members.slice(0, this.itemsPerPage);
       });
   }
-
 }
